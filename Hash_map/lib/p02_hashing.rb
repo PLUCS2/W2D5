b2280696698deeb21ba475f241458ed617d35b2c
App Academy Student <aa@Apps-Mac-mini-72.local>
2019-08-23 14:11:55 -0400

    p02_hashing completed

--- Hash_map/lib/p02_hashing.rb
+++ Hash_map/lib/p02_hashing.rb
@@ -4,11 +4,25 @@ end
 
 class Array
   def hash
+    answer = 5
+    count = 12
+    self.each do |ele|
+      answer %= (ele.hash + count)
+      count += 3
+    end
+    answer
   end
 end
 
 class String
   def hash
+    alph = ("a".."z").to_a
+    ans = []
+    self.split("").each do |letter|
+      idx = alph.index(letter)
+      ans << [idx].hash
+    end
+    ans.hash
   end
 end
 
@@ -16,6 +30,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sorted_ha = self.keys.sort 
+    sorted_ha += self.values
+    sorted_ha.hash
   end
 end
